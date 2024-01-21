f42018573cd7179a482f4ca0929eb847b40fbcf6
luis <felipe>
2024-01-21 19:55:30 +0100

    vercion 1.1 conexion exitosa

--- Documents/NetBeansProjects/proyecto_final/src/main/java/com/mycompany/proyecto_final/Proyecto_final.java
+++ Documents/NetBeansProjects/proyecto_final/src/main/java/com/mycompany/proyecto_final/Proyecto_final.java
@@ -11,6 +11,9 @@ package com.mycompany.proyecto_final;
 public class Proyecto_final {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
+        conexion ca = new conexion();
+        ca.conexion();
     }
 }
